c03c5ae96a4c35446b36ac05b13f833c5a685367
Yuvi_Developer <81914878+yuvi-coder@users.noreply.github.com>
2025-03-30 11:13:52 +0530

    Update StdInOut.java

--- StdInOut.java
+++ StdInOut.java
@@ -5,7 +5,7 @@ public class StdInOut {
         Scanner sc=new Scanner(System.in); //input scanner should be closed properly.
         int i=sc.nextInt();
         double d=sc.nextDouble();
-        sc.nextLine();//This line used to get the proper input of a string - search learn ? - without using this line error ? - find.
+        sc.nextLine();//This line used to get the proper input of a string.
         String s=sc.nextLine();
         System.out.println("String: "+s);
         System.out.println("Double: "+d);
