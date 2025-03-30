d98b323b21fdcb4a748a26a526a6e4cb6333ff08
Yuvi_Developer <81914878+yuvi-coder@users.noreply.github.com>
2025-03-30 16:09:52 +0530

    Create StdInOut.java

---
+++ Basics_of_Java/StdInOut.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;//specific needed functions should be used.
+public class StdInOut {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in); //input scanner should be closed properly.
+        int i=sc.nextInt();
+        double d=sc.nextDouble();
+        sc.nextLine();//This line used to get the proper input of a string - search learn ? - without using this line error ? - find.
+        String s=sc.nextLine();
+        System.out.println("String: "+s);
+        System.out.println("Double: "+d);
+        System.out.println("Integer: "+i);
+        sc.close();
+    }
+}
